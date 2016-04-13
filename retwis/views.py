3a4e05954012006bbaa9dbf1ed730db56932c721
Kushagra Mansingh <kush@Kushagras-MacBook-Pro.local>
2016-04-13 12:14:18 -0400

    Bootstrap forms and clean up code

--- retwis/views.py
+++ retwis/views.py
@@ -1,9 +1,10 @@
 import redis
 
 from flask import (g, jsonify, request, render_template,
-    session, redirect, url_for)
+                   session, redirect, url_for)
 
 from retwis import app
+
 
 def init_db():
     db = redis.StrictRedis(
@@ -12,11 +13,13 @@ def init_db():
         db=app.config['DB_NO'])
     return db
 
+
 @app.before_request
 def before_request():
     g.db = init_db()
 
-@app.route('/signup', methods=['GET','POST'])
+
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
     error = None
     if request.method == 'GET':
@@ -24,10 +27,11 @@ def signup():
     username = request.form['username']
     password = request.form['password']
     user_id = str(g.db.incrby('next_user_id', 1000))
-    g.db.hmset('user:'+user_id, dict(username=username, password=password))
+    g.db.hmset('user:' + user_id, dict(username=username, password=password))
     g.db.hset('users', username, user_id)
     session['username'] = username
-    return jsonify(**g.db.hgetall('user:'+user_id))
+    return redirect(url_for('home'))
+
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -40,20 +44,21 @@ def login():
     if not user_id:
         error = 'No such user'
         return render_template('login.html', error=error)
-    if password != g.db.hget('user:'+str(user_id), 'password'):
+    if password != g.db.hget('user:' + str(user_id), 'password'):
         error = 'Incorrect password'
         return render_template('login.html', error=error)
     session['username'] = username
     return redirect(url_for('home'))
+
 
 @app.route('/logout')
 def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
 
+
 @app.route('/home', methods=['GET'])
 def home():
-    app.logger.debug('Session %s', session)
-    if not session['username']:
+    if not session:
         return redirect(url_for('login'))
     return render_template('home.html')
